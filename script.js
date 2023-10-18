6094cfe77212e77a3f00e3bbd2b831a6fa77e39a
Yash Dafade <119852684+Yashdafade@users.noreply.github.com>
2023-10-18 22:39:22 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,183 @@
+let menuicon = document.getElementById("menu-icon");
+let nav = document.querySelector('.navbar');
+
+
+menuicon.addEventListener("click", () => {
+    menuicon.classList.toggle('bx-x');
+    if (nav.style.transform === "translateX(0%)") {
+        nav.style.transform = "translateX(-100%)";
+    } else {
+        nav.style.transform = "translateX(0%)";
+    }
+});
+
+//welcome animation
+const elementsToAnimate = document.querySelectorAll('.scroll-fade');
+const elementsToAnimate2 = document.querySelectorAll('.footer-animate');
+
+const isInViewport = (element) => {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+};
+
+const animateOnScroll = () => {
+    elementsToAnimate.forEach((element) => {
+        if (isInViewport(element) && !element.classList.contains('animated')) {
+            element.classList.add('animated');
+        }
+    });
+
+    elementsToAnimate2.forEach((element) => {
+        if (isInViewport(element) && !element.classList.contains('animated2')) {
+            element.classList.add('animated2');
+        }
+    });
+};
+
+window.addEventListener('scroll', animateOnScroll);
+window.addEventListener('load', animateOnScroll);
+
+
+
+
+//reveiw section
+'use strict'
+var	testim = document.getElementById("testim"),
+		testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+    testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+    testimLeftArrow = document.getElementById("left-arrow"),
+    testimRightArrow = document.getElementById("right-arrow"),
+    testimSpeed = 4500,
+    currentSlide = 0,
+    currentActive = 0,
+    testimTimer,
+		touchStartPos,
+		touchEndPos,
+		touchPosDiff,
+		ignoreTouch = 30;
+;
+
+window.onload = function() {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length-1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");            
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+    
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    testimLeftArrow.addEventListener("click", function() {
+        playSlide(currentSlide -= 1);
+    })
+
+    testimRightArrow.addEventListener("click", function() {
+        playSlide(currentSlide += 1);
+    })    
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+
+    // keyboard shortcuts
+    document.addEventListener("keyup", function(e) {
+        switch (e.keyCode) {
+            case 37:
+                testimLeftArrow.click();
+                break;
+                
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            case 39:
+                testimRightArrow.click();
+                break;
+
+            default:
+                break;
+        }
+    })
+		
+		testim.addEventListener("touchstart", function(e) {
+				touchStartPos = e.changedTouches[0].clientX;
+		})
+	
+		testim.addEventListener("touchend", function(e) {
+				touchEndPos = e.changedTouches[0].clientX;
+			
+				touchPosDiff = touchStartPos - touchEndPos;
+			
+				console.log(touchPosDiff);
+				console.log(touchStartPos);	
+				console.log(touchEndPos);	
+
+			
+				if (touchPosDiff > 0 + ignoreTouch) {
+						testimLeftArrow.click();
+				} else if (touchPosDiff < 0 - ignoreTouch) {
+						testimRightArrow.click();
+				} else {
+					return;
+				}
+			
+		})
+}
+  
+//submit-review
+    const form = document.getElementById("contactForm");
+    const successMessage = document.getElementById("successMessage");
+    
+    form.addEventListener("submit", function(event) {
+        event.preventDefault(); 
+        
+        // You can add AJAX code here to actually submit the form data to your server.
+
+        successMessage.style.display = "flex"; 
+        form.reset(); 
+    });
+
+
+
+  
+
+
+  
+
+
+
+
+
+
+
