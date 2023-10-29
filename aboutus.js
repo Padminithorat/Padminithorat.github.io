d697cccf674d30c6a1a124f9d814bb8a76a36d0e
Zainab <143055554+ZAIN1-123@users.noreply.github.com>
2023-10-29 16:57:50 +0530

    Update aboutus.js

--- aboutus.js
+++ aboutus.js
@@ -48,3 +48,20 @@ menuicon.addEventListener("click", () => {
     }
 });
 
+
+var text = document.getElementById('text');
+var newDom = '';
+var animationDelay = 6;
+
+for(let i = 0; i < text.innerText.length; i++)
+{
+    newDom += '<span class="char">' + (text.innerText[i] == ' ' ? '&nbsp;' : text.innerText[i])+ '</span>';
+}
+
+text.innerHTML = newDom;
+var length = text.children.length;
+
+for(let i = 0; i < length; i++)
+{
+    text.children[i].style['animation-delay'] = animationDelay * i + 'ms';
+}
